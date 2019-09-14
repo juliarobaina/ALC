0e94dd98944192e6e7ef5a74eb3c335407042d48
juliarobaina <47131530+juliarobaina@users.noreply.github.com>
2019-09-14 16:55:26 -0300

    Update mainSeidel.c

--- teste/teste2/mainSeidel.c
+++ teste/teste2/mainSeidel.c
@@ -1,9 +1,5 @@
 #include "seidel.c"
 
-int res(int n,int n2){
-	return n + n2;
-}
-
 int main(){
 
 	int numLinhas,numColunas,numIteracoes;
@@ -13,7 +9,7 @@ int main(){
 
 	int a[numLinhas][numColunas];//valores da matriz
 	int b[numLinhas];//valores depois da igualdade da equação
-	float x[numLinhas];//vetor solução inicial
+	int x[numLinhas];//vetor solução inicial
 
 	printf("Digite a matriz\n");
 	for (int i = 0; i < numLinhas; i++){
@@ -46,7 +42,7 @@ int main(){
 	printf("Digite o vetor inicial\n");
 	for (int i = 0; i < numLinhas; i++){
 		printf("Digite o %dº valor\n",i+1);
-		scanf("%f",&x[i]);
+		scanf("%d",&x[i]);
 	}
 
 	printf("\n");
@@ -57,123 +53,125 @@ int main(){
 	printf("\n");
 
 	int k = 0;
-	float *y = calloc(numLinhas,sizeof(float));
+	int *y = calloc(numLinhas,sizeof(int));
 	float temp;
 	int aux;
 
 	//Para fazer a fração
 	int auxMultN,auxMultD,auxDiviN,auxDiviD,auxSomaN,auxSomaD;
 	int **vetorsolucao = alocarMatriz(numLinhas,2);
+	int **matrizParaCalcular = alocarMatriz(2, 2);
+	int **vetorAuxiliar = alocarMatriz(1,2);
+	int **vetorAuxiliar2 = alocarMatriz(1,2);
+
+	int cont = 0;
+	int vetorAuxN, vetorAuxD;
 	
 	for (int i = 0; i < numLinhas; i++){
-		vetorsolucao[i][i] = x[i];
-		vetorsolucao[i][i+1] = 1;
+		vetorsolucao[i][0] = x[i];
+		vetorsolucao[i][1] = 1;
+		/*printf("o valor do x %d\n",x[i]);
+		printf("vetorsolucao[%d][0] = %d\n",i,vetorsolucao[i][0]);
+		printf("vetorsolucao[%d][1] = %d\n",i,vetorsolucao[i][1]);*/
 	}
-	int o = 0;
-
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
 	while(k < numIteracoes){
 		
 		for (int i = 0 ; i < numLinhas; i++){
 			y[i] = b[i];
+			cont = 0;
 			for (int j = 0; j < numLinhas; j++){
 				if(j != i){
-					if(i == 0){
-						auxMultN = multiplicarFracao(a[i][j],vetorsolucao[i][i],1,vetorsolucao[i][i+1],1);
-						auxMultD = multiplicarFracao(a[i][j],vetorsolucao[i][i],1,vetorsolucao[i][i+1],2);
-					
 
-					
-						printf("Entrei\n");
-						auxSomaN = somarFracao(y[i],auxMultN,1,auxMultD,1);
-						auxSomaD = somarFracao(y[i],auxMultN,1,auxMultD,2);
-						printf("AuxMult = %d -- %d\n",auxMultN,auxMultD);
-						printf("AuxSoma = %d -- %d\n",auxSomaN,auxSomaD);
-					}else if(i == 1){
-						//auxMultN = multiplicarFracao(a[i][j],vetorsolucao[i-1][i-1],1,vetorsolucao[i-1][i],1);
-						auxMultN = multiplicarFracao(vetorsolucao[i-1][i-1],a[i][j],vetorsolucao[i-1][i],1,1);
-						auxMultD = multiplicarFracao(vetorsolucao[i-1][i-1],a[i][j],vetorsolucao[i-1][i],1,2);
-
-						//auxSomaN = somarFracao(y[i],auxMultN,1,auxMultD,1);
-						auxSomaN = somarFracao(auxMultN,y[i],auxMultD,1,1);
-						auxSomaD = somarFracao(auxMultN,y[i],auxMultD,1,2);
-						printf("AuxMult = %d -- %d\n",auxMultN,auxMultD);
-						printf("AuxSoma = %d -- %d\n",auxSomaN,auxSomaD);
-					}else{
-						if(j == 1){
-							//auxMultN = multiplicarFracao(a[i][j],vetorsolucao[i-1][i-1],1,vetorsolucao[i-1][i],1);
-						auxMultN = multiplicarFracao(vetorsolucao[i-j][i-j],a[i][j],vetorsolucao[i-j][i],1,1);
-						auxMultD = multiplicarFracao(vetorsolucao[i-j][i-j],a[i][j],vetorsolucao[i-j][i],1,2);
-
-						auxSomaN = somarFracao(y[i],auxMultN,1,auxMultD,1);
-						//auxSomaN = somarFracao(auxMultN,y[i],auxMultD,1,1);
-						auxSomaD = somarFracao(y[i],auxMultN,1,auxMultD,2);
-						printf("AuxMult = %d -- %d\n",auxMultN,auxMultD);
-						printf("AuxSoma = %d -- %d\n",auxSomaN,auxSomaD);
-						}
+					/*if(i == 1){
+						printf("---------------------------------------\n");
+						printf("%d\n",vetorsolucao[j][0]);
+						printf("%d\n",vetorsolucao[j][1]);
 						
-					}
-					
-					
-					/*else{
-						printf("NAAAO\n");
-						auxSomaN = somarFracao(x[i],auxMultN,1,auxMultD,1);
-						auxSomaD = somarFracao(x[i],auxMultN,1,auxMultD,2);
+						printf("---------------------------------------\n");
 					}*/
-					
-					
-					//vetorsolucao[i][i] = auxSomaN;
-					//vetorsolucao[i][i+1] = auxSomaD;
 
-					y[i] = y[i] - a[i][j] * x[j];
+					matrizParaCalcular[cont][0] = multiplicarFracao((a[i][j] * -1),vetorsolucao[j][0],1,vetorsolucao[j][1],1);
+					matrizParaCalcular[cont][1] = multiplicarFracao((a[i][j] * -1),vetorsolucao[j][0],1,vetorsolucao[j][1],2);
+					
+					//matrizParaCalcular[cont][0] = somarFracao(y[i],matrizParaCalcular[cont][0],1,matrizParaCalcular[cont][1],1);
+					//matrizParaCalcular[cont][1] = somarFracao(y[i],matrizParaCalcular[cont][0],1,matrizParaCalcular[cont][1],2);
+					//printf("%d - cont %d\n",matrizParaCalcular[cont][0],cont);
+					//printf("%d - cont %d\n",matrizParaCalcular[cont][1],cont);
+					
+					/*printf("%d - cont %d\n",vetorsolucao[j][0],cont);
+					printf("%d - cont %d\n",vetorsolucao[j][1],cont);*/
+					//printf("%d - cont %d\n",matrizParaCalcular[cont][1],cont);
+
+					cont++;
+					//y[i] = y[i] - a[i][j] * x[j];
 				}
-
-
 				
 			}
 
-			if(i == 0){
-				vetorsolucao[i][i] = auxSomaN;
-				vetorsolucao[i][i+1] = a[i][i];
-			}else{
-				auxDiviN = dividirFracao(auxSomaN,a[i][i],auxSomaD,1,1);
-				auxDiviD = dividirFracao(auxSomaN,a[i][i],auxSomaD,1,2);
-				printf("AuxdiviN e: %d\n",auxSomaN);
-				vetorsolucao[i][i] = auxDiviN;
-				vetorsolucao[i][i+1] = auxDiviD;
-			}
+			/*auxSomaN = somarFracao(y[i],auxMultN,1,auxMultD,1);
+			matrizParaCalcular[cont][1] = somarFracao(y[i],auxMultN,1,auxMultD,2);*/
+			vetorAuxiliar[0][0] = somarFracao(matrizParaCalcular[0][0],matrizParaCalcular[1][0],matrizParaCalcular[0][1],matrizParaCalcular[1][1],1);
+					/*printf("%d\n",matrizParaCalcular[0][0]);
+					printf("%d\n",matrizParaCalcular[0][1]);
+					printf("%d\n",matrizParaCalcular[1][0]);
+					printf("%d\n",matrizParaCalcular[1][1]);
+					printf("&&&& %d \n",vetorAuxiliar[0][0]);*/
+			vetorAuxiliar[0][1] = somarFracao(matrizParaCalcular[0][0],matrizParaCalcular[1][0],matrizParaCalcular[0][1],matrizParaCalcular[1][1],2);
+					//printf("$$$$ %d \n",vetorAuxiliar[0][1]);
+
+			vetorAuxiliar2[0][0] = somarFracao(b[i],vetorAuxiliar[0][0],1,vetorAuxiliar[0][1],1);
+			vetorAuxiliar2[0][1] = somarFracao(b[i],vetorAuxiliar[0][0],1,vetorAuxiliar[0][1],2);
+				/*	printf("%d\n",b[i]);
+					printf("%d\n",vetorAuxiliar[0][0]);
+					printf("%d\n",vetorAuxiliar[0][1]);
+					
+					printf("&&&& %d \n",vetorAuxiliar[0][0]);
+			printf("VetorSuxiliar2 %d\n",vetorAuxiliar2[0][0]);
+			printf("VetorSuxiliar2 %d\n",vetorAuxiliar2[0][1]);*/
+
+			vetorsolucao[i][0] = dividirFracao(vetorAuxiliar2[0][0],a[i][i],vetorAuxiliar2[0][1],1,1);
+			//printf("Vetor Auxiliar %d\n",vetorsolucao[i][0]);
+			vetorsolucao[i][1] = dividirFracao(vetorAuxiliar2[0][0],a[i][i],vetorAuxiliar2[0][1],1,2);
 			
-		
-			
-			y[i] = y[i] / a[i][i];
+			printf("Iteracao %d -> Numerador: %d\n",k,vetorsolucao[i][0]);
+			printf("Iteraao %d -> Denominador: %d\n",k,vetorsolucao[i][1]);
+			/*y[i] = y[i] / a[i][i];
 			temp = x[i];
 			x[i] = y[i];
-			y[i] = temp;
+			y[i] = temp;*/
 		}
 
 		//critério de parada
 		k++;
 	}
 
-	for (int i = 0; i < numLinhas; i++){
+	/*for (int i = 0; i < numLinhas; i++){
 		printf("O vetor solucao e \n");
-		printf(" x[%d] = %f\n",i+1,x[i]);
-	}
+		printf(" x[%d] = %d\n",i+1,x[i]);
+	}*/
 
 	printf("\n");
 
 	printf("-------------------------------------------\n");
 	for (int i = 0; i < numLinhas; i++){
 		
-			printf(" vetSol[%d] = %d",i+1,vetorsolucao[i][i]);
+			printf(" vetSol[%d] = %d",i+1,vetorsolucao[i][0]);
 			printf("---\n");
-			printf("%10d\n",vetorsolucao[i][i+1]);
+			printf("%10d\n",vetorsolucao[i][1]);
 		
 	}
 
+	printf("\n");
+
 
 	free(y);
-
 	liberarMatriz(vetorsolucao,numLinhas);
+	liberarMatriz(matrizParaCalcular,2);
+	liberarMatriz(vetorAuxiliar,1);
 
 
 	return 0;
