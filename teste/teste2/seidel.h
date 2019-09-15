dc70fc341f1b4d563443193d2c07d0e3df0f4455
juliarobaina <47131530+juliarobaina@users.noreply.github.com>
2019-09-15 19:12:07 -0300

    Update seidel.h

--- teste/teste2/seidel.h
+++ teste/teste2/seidel.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct fracao{
+	int numerador;
+	int denominador;
+};
+
+typedef struct fracao Fracao;
+
 	int somarFracao(int numerador1,int numerador2,int denominador1,int denominador2,int escolha);
 	int dividirFracao(int numerador1,int numerador2,int denominador1,int denominador2,int escolha);
 	int multiplicarFracao(int numerador1,int numerador2,int denominador1,int denominador2,int escolha);
