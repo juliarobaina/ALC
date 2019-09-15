e6e7738729be2282251fa004a0194d3d079ae122
juliarobaina <47131530+juliarobaina@users.noreply.github.com>
2019-09-15 19:08:24 -0300

    Update main4.c

--- teste/teste2/main4.c
+++ teste/teste2/main4.c
@@ -3,7 +3,12 @@
 int main(){
 
 	int numLinhas,numColunas,numIteracoes;
-
+	//Para fazer a fração	
+	int **matrizParaCalcular = alocarMatriz(2, 2);
+	int cont = 0;
+	Fracao* fracaoSolucao =  malloc(numLinhas * sizeof(Fracao));//Primeiramente recebe o vetor inicial
+	Fracao fracaoAuxiliar;
+	Fracao fracaoAuxiliar2;
 	
 	printf("Digite o numero de linhas e colunas: ");
 	scanf("%d %d",&numLinhas,&numColunas);
@@ -15,8 +20,7 @@ int main(){
 
 	int a[numLinhas][numColunas];//valores da matriz
 	int b[numLinhas];//valores depois da igualdade da equação
-	int x[numLinhas];//vetor solução inicial
-
+	
 	printf("Digite a matriz\n");
 	for (int i = 0; i < numLinhas; i++){
 		for(int j = 0; j < numColunas;j++){
@@ -44,11 +48,12 @@ int main(){
 	}
 
 	printf("\n");
-
+	
 	printf("Digite o vetor inicial\n");
 	for (int i = 0; i < numLinhas; i++){
 		printf("Digite o %dº valor\n",i+1);
-		scanf("%d",&x[i]);
+		scanf("%d",&fracaoSolucao[i].numerador);
+		fracaoSolucao[i].denominador = 1;
 	}
 
 	printf("\n");
@@ -64,30 +69,18 @@ int main(){
 
 	int k = 0;
 
-	//Para fazer a fração	
-	int **matrizParaCalcular = alocarMatriz(2, 2);
-	
-
-	int cont = 0;
-	
-
-	Fracao* fracaoSolucao =  malloc(numLinhas * sizeof(Fracao));
-	Fracao fracaoAuxiliar;
-	Fracao fracaoAuxiliar2;
-	
-	for (int i = 0; i < numLinhas; i++){
-		fracaoSolucao[i].numerador = x[i];
-		fracaoSolucao[i].denominador = 1;
-	}
-	
 	while(k < numIteracoes){
 		
 		for (int i = 0 ; i < numLinhas; i++){
-			if(a[i][i] != 0){
-				cont = 0;
-				for (int j = 0; j < numLinhas; j++){
-					if(j != i){
 
+			if(a[i][i] != 0){//Não faz divisão por zero
+
+				cont = 0;
+
+				for (int j = 0; j < numLinhas; j++){
+					
+					if(j != i){
+						//faz a multiplicação do a[i][j] * x[j]
 						matrizParaCalcular[cont][0] = multiplicarFracao((a[i][j] * -1),fracaoSolucao[j].numerador,1,fracaoSolucao[j].denominador,1);
 						matrizParaCalcular[cont][1] = multiplicarFracao((a[i][j] * -1),fracaoSolucao[j].numerador,1,fracaoSolucao[j].denominador,2);
 						
@@ -96,50 +89,37 @@ int main(){
 					
 				}
 
-				if(numLinhas == 2){
+				if(numLinhas == 2){//Só tem uma variável no outro lado da equação
 
 					fracaoAuxiliar.numerador = matrizParaCalcular[0][0];
 					fracaoAuxiliar.denominador = matrizParaCalcular[0][1];
-
-					fracaoAuxiliar2.numerador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,1);
-					fracaoAuxiliar2.denominador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,2);
-
-					
-					fracaoSolucao[i].numerador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,1);
-					fracaoSolucao[i].denominador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,2);
-
-
-					int valorMDC = MDC(fracaoSolucao[i].numerador,fracaoSolucao[i].denominador);
-
-					fracaoSolucao[i].numerador /= valorMDC;
-					fracaoSolucao[i].denominador /= valorMDC;
-					
 						
 				}else if(numLinhas == 1){
 					
 					fracaoSolucao[i].numerador = dividirFracao(b[i],a[i][i],1,1,1);
 					fracaoSolucao[i].denominador = dividirFracao(b[i],a[i][i],1,1,2);
-					
+					break;
+
 				}else{
 
 					//somar x1 com x2, por exemplo
 					fracaoAuxiliar.numerador = somarFracao(matrizParaCalcular[0][0],matrizParaCalcular[1][0],matrizParaCalcular[0][1],matrizParaCalcular[1][1],1);					
-					fracaoAuxiliar.denominador = somarFracao(matrizParaCalcular[0][0],matrizParaCalcular[1][0],matrizParaCalcular[0][1],matrizParaCalcular[1][1],2);							
-
-					//somar o resultado anterior com o valor depois da equacao
-					fracaoAuxiliar2.numerador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,1);
-					fracaoAuxiliar2.denominador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,2);
-					
-					//dividir o resultado com o a[i][i]
-					fracaoSolucao[i].numerador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,1);					
-					fracaoSolucao[i].denominador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,2);
-					
-					int valorMDC = MDC(fracaoSolucao[i].numerador,fracaoSolucao[i].denominador);					
-					
-					fracaoSolucao[i].numerador /= valorMDC;
-					fracaoSolucao[i].denominador /= valorMDC;
-					
+					fracaoAuxiliar.denominador = somarFracao(matrizParaCalcular[0][0],matrizParaCalcular[1][0],matrizParaCalcular[0][1],matrizParaCalcular[1][1],2);												
 				}
+
+				//somar o resultado anterior com o valor depois da equacao
+				fracaoAuxiliar2.numerador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,1);
+				fracaoAuxiliar2.denominador = somarFracao(b[i],fracaoAuxiliar.numerador,1,fracaoAuxiliar.denominador,2);
+				
+				//dividir o resultado com o a[i][i]
+				fracaoSolucao[i].numerador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,1);					
+				fracaoSolucao[i].denominador = dividirFracao(fracaoAuxiliar2.numerador,a[i][i],fracaoAuxiliar2.denominador,1,2);
+				
+				//Calcula o máximo divisor comum
+				int valorMDC = MDC(fracaoSolucao[i].numerador,fracaoSolucao[i].denominador);					
+				//Faz a simplificação
+				fracaoSolucao[i].numerador /= valorMDC;
+				fracaoSolucao[i].denominador /= valorMDC;
 
 			}else{
 				exit(1);
