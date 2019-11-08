cdc35df847b520730e03c3b2b322259ce3c64073
juliarobaina <47131530+juliarobaina@users.noreply.github.com>
2019-11-08 20:58:45 -0300

    Update metodosDiretos.h

--- metodosDiretos/metodosDiretos.h
+++ metodosDiretos/metodosDiretos.h
@@ -2,11 +2,16 @@
 #define _METODOSDIRETOS_
 
 #include "alocacaoDinamica.c"
+#include <math.h>
 
+	//int verificarMatrizTriangular(double** matriz,int ordem);
 	double* triangularInferior(double** matriz, double* vetorB, double* vetorSolucao, int ordem);
 	double* triangularSuperior(double** matriz, double* vetorB, double* vetorSolucao, int ordem);
-	double** eliminacaoGauss(double** matriz, double* vetorB ,int ordem);
+	void eliminacaoGauss_pivoteamentoTotal(double** matriz, double* vetorB ,int ordem);
+	void eliminacaoGauss_pivoteamentoParcial(double** matriz, double* vetorB ,int ordem);
+	void eliminacaoGauss(double** matriz, double* vetorB ,int ordem);
 
 	void imprimirMatriz(double** matriz, int ordem);
+	void imprimirVetor(double* vet, int ordem);
 
 #endif
